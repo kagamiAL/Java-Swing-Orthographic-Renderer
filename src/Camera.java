870e77a0031b22e14199a72b8c6ea7d35e395b1b
Alan Bach <kagamibach@gmail.com>
2024-03-12 14:29:26 -0600

    Rendering without shading created

--- src/Camera.java
+++ src/Camera.java
@@ -1,3 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.util.Arrays;
+
 public class Camera {
 
     public static final Vector3 GLOBAL_UP = new Vector3(0, 1, 0);
@@ -10,11 +16,49 @@ public class Camera {
 
     private int width = 512;
 
+    private static JFrame frame;
+
+    private static JLabel label;
+
     public Camera(){}
 
     public Camera(int height, int width){
         this.height = height;
         this.width = width;
+    }
+
+    private static void display(BufferedImage image){
+        if(frame==null){
+            frame=new JFrame();
+            frame.setTitle("this_might_be_epic");
+            frame.setSize(image.getWidth(), image.getHeight());
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            label=new JLabel();
+            label.setIcon(new ImageIcon(image));
+            frame.getContentPane().add(label,BorderLayout.CENTER);
+            frame.setLocationRelativeTo(null);
+            frame.pack();
+            frame.setVisible(true);
+        }else label.setIcon(new ImageIcon(image));
+    }
+
+    private static boolean edgeFunction(Vector3 a, Vector3 b, Vector2 p){
+        return (a.x - b.x) * (p.y - a.y) - (a.y - b.y) * (p.x - a.x) >= 0;
+    }
+
+    private static Vector2[] getBoundingBoxMinMax(Vector3[] vertices, int[] face){
+        Vector2 boxMin = new Vector2(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        Vector2 boxMax = new Vector2(Integer.MIN_VALUE, Integer.MIN_VALUE);
+
+        for (int j : face) {
+            Vector3 vertex = vertices[j];
+            if (vertex.x < boxMin.x) boxMin.x = vertex.x;
+            if (vertex.y < boxMin.y) boxMin.y = vertex.y;
+            if (vertex.x > boxMax.x) boxMax.x = vertex.x;
+            if (vertex.y > boxMax.y) boxMax.y = vertex.y;
+        }
+
+        return new Vector2[]{boxMin, boxMax};
     }
 
     private Vector3[] getProjectedVertices(Vector3[] vertices, int scale){
@@ -38,9 +82,31 @@ public class Camera {
     }
 
     public void render(Item3D item3D){
-        Vector3[] projectedVertices = getProjectedVertices(item3D.getVertices());
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        WritableRaster raster = bufferedImage.getRaster();
+        Vector3[] projectedVertices = getProjectedVertices(item3D.getVertices(), item3D.getScale());
+        int[] frameBuffer = new int[width * height];
+        Arrays.fill(frameBuffer, Color.white.getRGB());
         for (int[] face: item3D.getFaces()){
-
+            Vector2[] boxMinMax = getBoundingBoxMinMax(projectedVertices, face);
+            int xMin = (int) Math.max(0, Math.min(width - 1, Math.floor(boxMinMax[0].x)));
+            int yMin = (int) Math.max(0, Math.min(height - 1, Math.floor(boxMinMax[0].y)));
+            int xMax = (int) Math.max(0, Math.min(width - 1, Math.floor(boxMinMax[1].x)));
+            int yMax = (int) Math.max(0, Math.min(height - 1, Math.floor(boxMinMax[1].y)));
+            for (int y = yMin; y <= yMax; y++){
+                for (int x = xMin; x <= xMax; x++){
+                    boolean inside = true;
+                    Vector2 pixel = new Vector2(x, y);
+                    inside &= edgeFunction(projectedVertices[face[0]], projectedVertices[face[1]], pixel);
+                    inside &= edgeFunction(projectedVertices[face[1]], projectedVertices[face[2]], pixel);
+                    inside &= edgeFunction(projectedVertices[face[2]], projectedVertices[face[0]], pixel);
+                    if (inside){
+                        frameBuffer[y * width + x] = Color.black.getRGB();
+                    }
+                }
+            }
         }
+        raster.setDataElements(0, 0, width, height, frameBuffer);
+        display(bufferedImage);
     }
 }
