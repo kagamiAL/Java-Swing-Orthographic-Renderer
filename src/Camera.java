2688e0054ebaa7378c5a1cd3aca3e9cc3634921a
Alan Bach <kagamibach@gmail.com>
2024-03-12 11:08:03 -0600

    Camera points in negative z axis

--- src/Camera.java
+++ src/Camera.java
@@ -1,12 +1,10 @@
-import java.util.Arrays;
-
 public class Camera {
 
     public static final Vector3 UP_VECTOR = new Vector3(0, 1, 0);
 
     public static final Vector3 RIGHT_VECTOR = new Vector3(1, 0, 0);
 
-    private Vector3 lookVector = new Vector3(0, 0, 1);
+    private Vector3 lookVector = new Vector3(0, 0, -1);
 
     private int height = 512;
 
