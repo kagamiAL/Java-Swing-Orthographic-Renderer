1c2b7c9bd62856f48d9059ade9e985467e6c394e
Alan Bach <kagamibach@gmail.com>
2024-03-13 20:58:58 -0600

    Main is general

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        Item3D item3D = ParseOBJ.parseObjFile("icosahedron.obj");
+    public static void main(String[] args) throws InterruptedException {
+        final int SCALE = 5;
+
+        Item3D item3D = ParseOBJ.parseObjFile("item.obj");
         if (item3D != null){
-            Camera camera = new Camera();
-            camera.render(item3D);
+            item3D.setScale(SCALE);
+            Camera camera = new Camera(1000, 1000);
+            Vector3 lookVector = new Vector3(0, 0, -1);
+            double step = 0;
+            while (true) {
+                lookVector.x = (float) (Math.sin(step) * 1);
+                camera.setLookVector(lookVector);
+                camera.render(item3D);
+                Thread.sleep(1);
+                step += 0.01;
+            }
         }
     }
 }
