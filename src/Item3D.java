b7329cc2f34cd2335f0b9d879776b92c0794a717
Alan Bach <kagamibach@gmail.com>
2024-03-28 15:07:10 -0600

    Faces optimisation + texture

--- src/Item3D.java
+++ src/Item3D.java
@@ -11,27 +11,12 @@ public class Item3D {
 
     private Texture texture = null;
 
-    private Color[] texturePointsColor = null;
-
-    public Item3D(Vector3[] vertices, int[][] faces){
+    public Item3D(Vector3[] vertices, Face[] faces){
         this.vertices = vertices;
         this.projectedVertices = new Vector3[vertices.length];
-        this.faces = new Face[faces.length];
-        for (int i = 0; i < faces.length; i++) {
-            this.faces[i] = new Face(faces[i], vertices, this.projectedVertices);
-        }
-    }
-
-    public void setTexture(Texture texture, ArrayList<double[]> texturePoints){
-        this.texture = texture;
-        texturePointsColor = new Color[texturePoints.size()];
-        for (int i = 0; i < texturePoints.size(); i++) {
-            double[] pix = texturePoints.get(i);
-            double pixelXCoordinate = pix[0] * texture.getWidth() - 0.5;
-            double pixelYCoordinate = (1-pix[1]) * texture.getHeight() - 0.5;
-            int x = (int) Math.floor(pixelXCoordinate);
-            int y = (int) Math.floor(pixelYCoordinate);
-            texturePointsColor[i] = new Color(texture.getRGBAt(x, y), true);
+        this.faces = faces;
+        for (Face face: faces){
+            face.setProjectedVertices(this.projectedVertices);
         }
     }
 
@@ -53,10 +38,11 @@ public class Item3D {
         return projectedVertices;
     }
 
-    public Color getColorAt(int index){
-        if (texture != null){
-            return texturePointsColor[index];
-        }
-        return Color.WHITE;
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+
+    public Texture getTexture() {
+        return texture;
     }
 }
